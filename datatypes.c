f98a5db30d9813265297c563734f2f1929b9ac14
chethanyadav456 <chethanyadav456@gmail.com>
2024-11-08 22:15:52 +0530

    first commit

---
+++ datatypes.c
@@ -0,0 +1,16 @@
+// Program to demonstrate basic data types in C
+#include <stdio.h>
+
+int main(void) {
+    int Integer = 10;
+    float Float = 10.5;
+    double Double = 10.5;
+    char Character = 'A';
+
+    printf("Integer: %d\n", Integer);
+    printf("Float: %f\n", Float);
+    printf("Double: %lf\n", Double);
+    printf("Character: %c\n", Character);
+
+    return 0;
+}
